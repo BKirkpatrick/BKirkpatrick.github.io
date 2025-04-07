94630cc039e6389d03d85a76a3ac13419bfb0cb6
Blair Kirkpatrick <6220300+BKirkpatrick@users.noreply.github.com>
2025-04-07 17:20:15 +0100

    rss blog

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Blog() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@blairckirkpatrick')
+      .then(response => {
+        setPosts(response.data.items);
+      })
+      .catch(error => {
+        console.error('Failed to fetch Medium posts', error);
+      });
+  }, []);
+
   return (
     <div>
       <h1>Blog</h1>
-      <p>Check out my writing on <a href="https://medium.com/@blairckirkpatrick" target="_blank" rel="noopener noreferrer">Medium</a>.</p>
+      {posts.map((post) => (
+        <div key={post.guid} style={{ marginBottom: '1.5rem' }}>
+          <h3><a href={post.link} target="_blank" rel="noopener noreferrer">{post.title}</a></h3>
+          <p dangerouslySetInnerHTML={{ __html: post.description.substring(0, 200) + '...' }} />
+        </div>
+      ))}
     </div>
   );
 }
